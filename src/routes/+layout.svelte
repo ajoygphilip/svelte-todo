e564ca9e8f9c60b234203c5b2a859e1358c48c11
ajoy <ajoygp551@gmail.com>
2025-02-23 00:01:37 +0530

    feat: add skelton layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import '../app.css';
+  let { children } = $props();
+  import { ModeWatcher } from 'mode-watcher';
+  import * as Menubar from '$lib/components/ui/menubar';
+  import Sun from 'svelte-radix/Sun.svelte';
+  import Moon from 'svelte-radix/Moon.svelte';
+  import { toggleMode } from 'mode-watcher';
+  import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<ModeWatcher />
+
+<div class="flex justify-between items-center rounded-none px-8 mb-8 py-4 border-y-[1px]">
+  <div class="flex">
+    <p>App Name</p>
+  </div>
+  <Button variant="ghost" on:click={toggleMode} class="justify-self-end">
+    <Sun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+    <Moon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    <span class="sr-only">Toggle theme</span>
+  </Button>
+</div>
+
+<div class="p-8 m-8 flex justify-center items-center">
+  {@render children()}
+</div>
