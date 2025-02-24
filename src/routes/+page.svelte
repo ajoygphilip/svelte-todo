bd94f64d0b03004af701434619674f5eca3fce1b
ajoy <ajoygp551@gmail.com>
2025-02-24 08:29:20 +0530

    feat: add todo type

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
-  let todos: string[] = $state(['I want to ', 'Build', 'something cool']);
+  import type { Todo } from '$lib/types';
+
+  const todos: Todo[] = $state([
+    { title: 'Buy milk... or a cow? 🤔', completed: true },
+    { title: 'Learn how to say no... starting tomorrow.', completed: false },
+    { title: 'Water the plants before they become cacti. 🌵', completed: false },
+    { title: "Google 'How to be productive'... later.", completed: false },
+    { title: 'Remind myself to stop forgetting things.', completed: false },
+    { title: "Take a nap and call it 'self-care'. 😴", completed: false },
+    { title: 'Convince my cat that I’m in charge. 🐱', completed: false },
+    { title: 'Actually read the terms & conditions. (Just kidding.)', completed: false },
+    { title: "Put 'wake up' on the list so I can cross something off.", completed: false }
+  ]);
 
   function addTodos(e: Event) {
     e.preventDefault();
     const form = e.target as HTMLFormElement;
     const input = form.querySelector('input') as HTMLInputElement;
-    let todo = input?.value;
-    todos.push(todo);
-    todos = todos;
+    let title = input?.value;
+    todos.push({ title, completed: false });
     input.value = '';
   }
 </script>
